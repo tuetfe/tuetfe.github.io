e10bac9841dc4d9ab4bcb56a947461974a140fd4
tuetfe <nalinia@mail.ru>
2016-11-27 22:50:42 +0300

    схематично все кадры

--- seasons/gulpfile.js
+++ seasons/gulpfile.js
@@ -1,33 +1,104 @@
 "use strict";
 
-var gulp = require("gulp");
+var gulp = require("gulp"); 
+var rename = require("gulp-rename");
 var plumber = require("gulp-plumber");
-var postcss = require("gulp-postcss");
 var precss = require("precss");
+var minify = require("gulp-csso");
+var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
+var mqpacker = require("css-mqpacker");
+var svgstore = require("gulp-svgstore");
+var svgmin = require("gulp-svgmin");
+var imagemin = require("gulp-imagemin");
 var server = require("browser-sync");
+var run = require("run-sequence");
+var del = require("del");
+//var jade = require("gulp-jade");
 
-gulp.task("style", function() {
-  gulp.src("postcss/style.css")
+// чтобы запустить эту задачу, наберите в командной строке gulp jade
+//gulp.task("jade", function() {
+//    return gulp.src("src/templates/**/*.jade")
+//        .pipe(jade()) 
+//        .pipe(gulp.dest("build"));
+//});
+
+gulp.task("style", function() { 
+  gulp.src("src/postcss/style.css")
     .pipe(plumber())
     .pipe(postcss([
       precss(),
       autoprefixer({browsers: [
         "last 3 version"
-      ]})
+      ]}),
+      mqpacker({
+        sort: true
+      })
     ]))
-    .pipe(gulp.dest("css"))
-    .pipe(server.reload({stream: true}));
+    .pipe(gulp.dest("build/css")) 
+    .pipe(minify()) 
+    .pipe(rename("style.min.css")) 
+    .pipe(gulp.dest("build/css"));
 });
 
-gulp.task("serve", ["style"], function() {
+gulp.task("images", function() {
+  return gulp.src("build/img/**/*.{png,jpg,gif}")
+    .pipe(imagemin([
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.jpegtran({progressive: true})
+    ]))
+  .pipe(gulp.dest("build/img"));
+});
+
+gulp.task("symbols", function() {
+  return gulp.src("build/img/icons/*.svg")
+    .pipe(svgmin())
+    .pipe(svgstore({
+      inlineSvg: true
+    }))
+    .pipe(rename("symbols.svg"))
+    .pipe(gulp.dest("build/img"));
+});
+
+gulp.task("serve", function() {
   server.init({
-    server: ".",
-    notify: false,
-    open: true,
-    ui: false
+    server: "build",
   });
-
-  gulp.watch("postcss/**/*.css", ["style"]);
-  gulp.watch("*.html").on("change", server.reload);
+  
+  gulp.watch("src/postcss/**/*.css", ["style"]);
+  gulp.watch("src/**/*.html").on("change", server.reload);
 });
+
+gulp.task("build", function(fn) {
+  run(
+    "clean", 
+    "copy",
+ //   "jade",
+    "style", 
+    "images", 
+    "symbols", 
+    fn 
+  );
+});
+
+gulp.task("copy", function() { 
+  return gulp.src([ 
+    "src/fonts/**/*.{woff,woff2,ttf,eot,svg}", 
+    "src/img/**", 
+    "src/js/**", 
+    "src/**/*.html" 
+  ], {
+    base: "src"
+  }) 
+    .pipe(gulp.dest("build/")); 
+});
+
+gulp.task("clean", function() { 
+  return del("build"); 
+});
+
+
+
+
+
+
